2dbcd51d3d41b233312172e5755555e3eb14883b
Jony635 <jony635molina@gmail.com>
2017-10-11 19:37:36 +0200

    Starting Module Player

---
+++ Ultimate Pixelerator/Motor2D/j1Player.cpp
@@ -0,0 +1,21 @@
+#include "j1Player.h"
+
+j1Player::j1Player() : j1Module()
+{
+	name.create("player");
+}
+
+// Destructor
+j1Player::~j1Player()
+{}
+
+bool j1Player::Awake(pugi::xml_node& playernode)
+{
+	
+
+}
+
+
+
+
+
---
+++ Ultimate Pixelerator/Motor2D/j1Player.h
@@ -0,0 +1,51 @@
+#ifndef __J1PLAYER__H
+#define __J1PLAYER__H
+
+#include "j1App.h"
+#include "j1Module.h"
+
+class j1Player : public j1Module
+{
+
+public:
+
+	j1Player();
+
+	// Destructor
+	virtual ~j1Player();
+
+
+
+	//Before render avaiable
+	bool Awake(pugi::xml_node&);
+
+	//Before first frame
+	bool Start();
+
+
+	//Before each frame
+	bool PreUpdate();
+
+	//Each frame
+	bool Update(float dt);
+
+	//After Each frame
+	bool PostUpdate();
+
+	//CleanUp
+	bool CleanUp();
+
+	//Saving and Loading
+	bool Load(pugi::xml_node&);
+		
+	bool Save(pugi::xml_node&) const;
+
+private:
+	p2Point<int>StartingPosition;
+	int speed;
+};
+
+
+
+
+#endif
