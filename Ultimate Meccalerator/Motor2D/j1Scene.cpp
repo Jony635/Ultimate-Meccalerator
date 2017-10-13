68a25bb8c9aa2bf26993f9d94bce03f5e22a3d68
Jony635 <jony635molina@gmail.com>
2017-10-13 18:58:40 +0200

    Moving camera manually disabled

--- Ultimate Meccalerator/Motor2D/j1Scene.cpp
+++ Ultimate Meccalerator/Motor2D/j1Scene.cpp
@@ -50,18 +50,6 @@ bool j1Scene::Update(float dt)
 	if(App->input->GetKey(SDL_SCANCODE_K) == KEY_DOWN)
 		App->SaveGame();
 
-	if(App->input->GetKey(SDL_SCANCODE_UP) == KEY_REPEAT)
-		App->render->camera.y += 1;
-
-	if(App->input->GetKey(SDL_SCANCODE_DOWN) == KEY_REPEAT)
-		App->render->camera.y -= 1;
-
-	if(App->input->GetKey(SDL_SCANCODE_LEFT) == KEY_REPEAT)
-		App->render->camera.x += 1;
-
-	if(App->input->GetKey(SDL_SCANCODE_RIGHT) == KEY_REPEAT)
-		App->render->camera.x -= 1;
-
 	//App->render->Blit(img, 0, 0);
 	App->map->Draw();
 	int mousex, mousey;
